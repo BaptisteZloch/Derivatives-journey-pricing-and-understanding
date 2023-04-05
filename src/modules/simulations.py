d7edefb11e2e9a403cafa79a68c23bf7f338a12b
BaptisteZloch <bzloch@hotmail.fr>
2023-04-05 22:56:01 +0200

    simulations in progress

---
+++ src/modules/simulations.py
@@ -0,0 +1,57 @@
+import numpy as np
+import numpy.typing as npt
+from typing import Literal
+import matplotlib.pyplot as plt
+
+
+def generate_brownian_paths(
+    n_paths: int,
+    n_steps: int,
+    T: int,
+    mu: float | int,
+    sigma: float | int,
+    s0: float | int,
+    brownian_type: Literal["ABM", "GBM"] = "ABM",
+    get_time: bool = True,
+) -> npt.NDArray[np.float64] | tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    dt = T / n_steps
+    paths = np.zeros((n_paths, n_steps + 1))
+    paths[:, 0] = np.log(s0)
+
+    for i in range(0, n_paths):
+        for j in range(n_steps):
+            paths[i, j + 1] = (
+                paths[i, j]
+                + (mu * 0.5 * sigma**2) * dt
+                + sigma * np.random.normal(0, np.sqrt(dt))
+            )
+
+    if brownian_type == "GBM":
+        paths = np.exp(paths)
+
+    return np.linspace(0, T, n_steps + 1), paths if get_time is True else paths
+
+
+def plot_paths(
+    time: npt.NDArray[np.float64],
+    paths: npt.NDArray[np.float64],
+    title: str = "Simulated Brownian Motion",
+) -> None:
+    fig = plt.figure(figsize=(15, 4))
+    for y in paths:
+        plt.plot(time, y)
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.grid(True)
+    plt.title(title)
+
+
+# n_sim = 100
+# n_step = 100
+# mu = 3
+# sigma = 0.5
+# s0 = 100
+
+# time, sim =  generate_brownian_paths(n_sim, n_step, 1, mu, sigma, s0, "ABM")
+
+# plot_paths(time, sim, f"Arithmetic Brownian Motion {n_sim} simulations, {n_step} steps, mu={mu}, sigma={sigma}, s0={s0}")
